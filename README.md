631e804a2df94eca38bcb7d5053805332d7e4660
Doarakko <2wua4nlyi@gmail.com>
2019-06-30 09:28:16 +0900

    remove reminder

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Kaggle Competition Notification
 [![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
 
-Notify new competition and remind one month, one week, three days, one day before the end of competition to Slack or LINE without coding.
+Notify new competition to Slack or LINE without coding.
 
 ## Requirements
 - Kaggle API
@@ -24,29 +24,14 @@ Set `FREQUENCY` with `Daily`.
 
 ![](img/set-schedule.png)
 
-
+If you get at short intervals, please fork and correct the program.
 ```
 now = datetime.datetime.utcnow()
 if self.start_date >= now - datetime.timedelta(days=ONE_DAY):
     self.notify_message = NEW_COMPETITION
-
-elif self.end_date <= now + datetime.timedelta(days=ONE_DAY):
-    self.notify_message = ONE_DAY_BEFORE
-
-elif self.end_date <= now + datetime.timedelta(days=THREE_DAYS) and self.end_date >= now + datetime.timedelta(days=THREE_DAYS-1):
-    self.notify_message = THREE_DAYS_BEFORE
-
-elif self.end_date <= now + datetime.timedelta(days=ONE_WEEK) and self.end_date >= now + datetime.timedelta(days=ONE_WEEK-1):
-    self.notify_message = ONE_WEEK_BEFORE
-
-elif self.end_date <= now + datetime.timedelta(days=ONE_MONTH) and self.end_date >= now + datetime.timedelta(days=ONE_MONTH-1):
-    self.notify_message = ONE_MONTH_BEFORE
-
 else:
     self.notify_message = DO_NOT_NOTIFY
 ```
-
-If you get at short intervals or change the schedule to remind, please fork and correct the program.
 
 ## Sample
 - Slack
--- main.py
+++ main.py
@@ -9,15 +9,7 @@ from kaggle import KaggleApi
 
 
 DO_NOT_NOTIFY = 'Do not notify this competition'
-NEW_COMPETITION = 'New competition is launched'
-ONE_MONTH_BEFORE = '1 month before the end of this competition'
-ONE_WEEK_BEFORE = '1 week before the end of this competition'
-THREE_DAYS_BEFORE = '3 days before the end of this competition'
-ONE_DAY_BEFORE = '1 day before the end of this competition'
-ONE_DAY = 1
-THREE_DAYS = 3
-ONE_WEEK = 7
-ONE_MONTH = 30
+NEW_COMPETITION = 'New kaggle competition is launched'
 
 # dotenv.load_dotenv('.env')
 POST = os.environ['POST']
@@ -54,21 +46,8 @@ class Competition:
 
         # assume to run once a day
         now = datetime.datetime.utcnow()
-        if self.start_date >= now - datetime.timedelta(days=ONE_DAY):
+        if self.start_date >= now - datetime.timedelta(days=1):
             self.notify_message = NEW_COMPETITION
-
-        elif self.end_date <= now + datetime.timedelta(days=ONE_DAY):
-            self.notify_message = ONE_DAY_BEFORE
-
-        elif self.end_date <= now + datetime.timedelta(days=THREE_DAYS) and self.end_date >= now + datetime.timedelta(days=THREE_DAYS-1):
-            self.notify_message = THREE_DAYS_BEFORE
-
-        elif self.end_date <= now + datetime.timedelta(days=ONE_WEEK) and self.end_date >= now + datetime.timedelta(days=ONE_WEEK-1):
-            self.notify_message = ONE_WEEK_BEFORE
-
-        elif self.end_date <= now + datetime.timedelta(days=ONE_MONTH) and self.end_date >= now + datetime.timedelta(days=ONE_MONTH-1):
-            self.notify_message = ONE_MONTH_BEFORE
-
         else:
             self.notify_message = DO_NOT_NOTIFY
 
@@ -79,10 +58,10 @@ def get_notify_competitions_list():
         api.authenticate()
 
         competitions_list = []
-        for info in api.competitions_list():
+        for info in api.competitions_list(sort_by='recentlyCreated'):
             competition = Competition(info)
-
-            if competition.user_enter and competition.notify_message != DO_NOT_NOTIFY:
+            
+            if competition.notify_message != DO_NOT_NOTIFY:
                 competitions_list.append(competition)
 
         return competitions_list
@@ -134,11 +113,12 @@ def post_line(competition):
 
 if __name__ == "__main__":
     competitions_list = get_notify_competitions_list()
-
-    for competition in competitions_list:
-        if POST == 'slack':
-            post_slack(competition)
-        elif POST == 'line':
-            post_line(competition)
-        else:
-            logger.error('POST(Notification destination) is invalid')
+    
+    if competitions_list is not None:
+        for competition in competitions_list:
+            if POST == 'slack':
+                post_slack(competition)
+            elif POST == 'line':
+                post_line(competition)
+            else:
+                logger.error('POST is invalid')
